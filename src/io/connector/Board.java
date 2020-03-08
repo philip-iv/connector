a141c1efc1f3f70cb5d548c476c57df18d159398
Philip Stephenson <philipstephenson4@gmail.com>
2020-03-08 19:42:21 -0400

    Fix board layout

--- src/io/connector/Board.java
+++ src/io/connector/Board.java
@@ -4,7 +4,7 @@ public class Board {
 	private Piece[][] board;
 	
 	public Board() {
-		board = new Piece[6][7];
+		board = new Piece[7][6]; // col, row
 	}
 	
 	public boolean addPiece(int col, Player owner) {
@@ -12,7 +12,7 @@ public class Board {
 		 * Attempts to add a new piece owned by owner in the specified column. Columns fill bottom-up.
 		 * If the column is full, the method returns false. Otherwise, it returns true.
 		 */
-		for (int i = 0; i <= 7; i++) {
+		for (int i = 0; i <= board[col].length; i++) {
 			if (board[col][i] == null) {
 				board[col][i] = new Piece(owner);
 				return true;
