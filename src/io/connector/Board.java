2fa603315bad8729356800eb1387304636f8d248
Philip Stephenson <philipstephenson4@gmail.com>
2020-03-08 19:38:11 -0400

    Rough in game objects

--- src/io/connector/Board.java
+++ src/io/connector/Board.java
@@ -1,11 +1,23 @@
 package io.connector;
 
-import io.connector.*;
-
 public class Board {
 	private Piece[][] board;
 	
 	public Board() {
-		board = Piece[6][7];
+		board = new Piece[6][7];
+	}
+	
+	public boolean addPiece(int col, Player owner) {
+		/**
+		 * Attempts to add a new piece owned by owner in the specified column. Columns fill bottom-up.
+		 * If the column is full, the method returns false. Otherwise, it returns true.
+		 */
+		for (int i = 0; i <= 7; i++) {
+			if (board[col][i] == null) {
+				board[col][i] = new Piece(owner);
+				return true;
+			}
+		}
+		return false;
 	}
 }
